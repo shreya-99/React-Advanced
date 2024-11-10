1d92715810c4668d7c0344e75fa63541d42024ad
shreya-99 <38156124+shreya-99@users.noreply.github.com>
2024-11-10 15:39:08 +0530

    src folder changes

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import Starter from './tutorial/03-conditional-rendering/starter/05-short-circuit-examples'
+import Final from './tutorial/01-useState/final/03-useState-array'
+
+function App() {
+  return (
+    <div className='container'>
+      <Starter/>
+      {/* <Final/> */}
+    </div>
+  );
+}
+
+export default App;
